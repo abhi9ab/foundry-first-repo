bf1d45369809cdbc1ae24b2a950f0faabf3df7af
abhi9ab <abhinabdas004@gmail.com>
2024-05-15 23:27:50 +0530

    commit

---
+++ script/interactions.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+//Fund 
+//Withdraw
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract FundFundMe is Script {
+    function run() external {
+
+    }
+}
+
+contract WithdrawFundMe is Script {}
